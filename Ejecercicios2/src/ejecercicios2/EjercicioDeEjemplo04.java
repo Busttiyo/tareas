a98987be61175544bb38115845b5e21ea093e245
Jonathan David Bustto <David.Bustto@hotmail.com>
2024-01-22 03:09:49 -0300

    ejercicios de ejemplo de la guia 2

---
+++ Ejecercicios2/src/ejecercicios2/EjercicioDeEjemplo04.java
@@ -0,0 +1,23 @@
+/*
+Escriba un programa que lea 20 números. Si el número leído es igual a cero se 
+debe salir del bucle y mostrar el mensaje "Se capturó el numero cero".  
+El programa deberá calcular y mostrar el resultado de la suma de los números 
+leídos, pero si el número es negativo no debe sumarse. 
+Nota: recordar el uso de la sentencia break.
+ */
+package ejecercicios2;
+
+/**
+ *
+ * @author david
+ */
+public class EjercicioDeEjemplo04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
